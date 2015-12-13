1d892a5352713508d197c3043d0fc057b083df3c
Arturo Gomez <artgmz@gmail.com>
2015-12-12 22:33:55 -0800

    Insert sample quizzes on server start.

---
+++ collections/quizzes.js
@@ -0,0 +1,2 @@
+// Quizzes collection. Quizzes consist of name, description, and questions.
+Quizzes = new Mongo.Collection('quizzes');
---
+++ server/main.js
@@ -0,0 +1,18 @@
+// Run on server start-up.
+Meteor.startup(function () {
+  var basicArithmeticQuiz = {};
+  var nflTriviaQuiz = {};
+  var quizzes = [];
+
+  // If there are no quizzes in the database, insert the sample quizzes.
+  if (Quizzes.find().count() === 0) {
+    basicArithmeticQuiz = JSON.parse(Assets.getText('quizzes/basic-arithmetic.json'));
+    nflTriviaQuiz = JSON.parse(Assets.getText('quizzes/nfl-trivia.json'));
+
+    quizzes = [basicArithmeticQuiz, nflTriviaQuiz];
+
+    _.each(quizzes, function (quiz) {
+      Quizzes.insert(quiz);
+    });
+  }
+});

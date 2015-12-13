b446dff89bbd875604c1ad9f1b57d1ea53c17479
Arturo Gomez <artgmz@gmail.com>
2015-12-12 19:27:20 -0800

    Create basic layout to be used app-wide.

--- client/routes.js
+++ client/routes.js
@@ -1,3 +1,10 @@
+Router.configure({
+  layoutTemplate: 'main',
+  onAfterAction: function () {
+    document.title = 'inquisitor!';
+  }
+});
+
 Router.route('/', {
   name: 'home',
   template: 'home'

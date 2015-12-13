28f5513a67d0448e114d47885a90be635861e17e
Arturo Gomez <artgmz@gmail.com>
2015-12-13 03:48:22 -0800

    Implement taking a quiz and viewing the results once done.

--- client/inquisitor.js
+++ client/inquisitor.js
@@ -6,5 +6,103 @@ Template.quizCard.helpers({
   }
 });
 
+// Quiz Card partial template events.
+Template.quizCard.events({
+  'click .quiz': function () {
+    var self = this;
+    Router.go('quiz', { _id: self._id });
+  }
+});
+
+// Take Quiz view template data setup.
+Template.quiz.onCreated(function () {
+  var self = this;
+
+  self.currentQuestionIdx = new ReactiveVar(0);
+
+  Session.set('disableNextStepButton', true);
+  Session.set('numCorrectAnswers', 0);
+});
+
+// Take Quiz view template helpers.
+Template.quiz.helpers({
+  disableNextStepButton: function () {
+    return Session.get('disableNextStepButton');
+  },
+  currentQuestion: function () {
+    var self = this;
+    return self.questions && self.questions[Template.instance().currentQuestionIdx.get()];
+  },
+  currentQuestionNum: function () {
+    return Template.instance().currentQuestionIdx.get() + 1;
+  },
+  nextStepButtonLabel: function () {
+    var currentQuestionNum = Template.instance().currentQuestionIdx.get() + 1;
+    var totalQuestions = self.questions && self.questions.length;
+
+    return (currentQuestionNum === totalQuestions) ? 'Finish' : 'Continue';
+  },
+  totalQuestions: function () {
+    var self = this;
+    return self.questions && self.questions.length;
+  }
+});
+
+// Take Quiz view template events.
+Template.quiz.events({
+  'click #nextStep': function (event, template) {
+    var self = this;
+    var chosenAnswer = template.find('input:radio:checked').value;
+    var currentQuestionIdx = Template.instance().currentQuestionIdx.get();
+    var correctAnswer = self.questions[currentQuestionIdx].answer;
+    var totalQuestions = self.questions && self.questions.length;
+
+    if (chosenAnswer === correctAnswer) {
+      Session.set('numCorrectAnswers', Session.get('numCorrectAnswers') + 1);
+    }
+
+    if ((currentQuestionIdx + 1) === totalQuestions) {
+      Router.go('quiz-results', { _id: self._id });
+    }
+    else {
+      // Disable button for next question.
+      Session.set('disableNextStepButton', true);
+
+      // Radio buttons for next question.
+      _.each(document.getElementsByName('answerOption'), function (radioButton) {
+        radioButton.checked = false;
+      });
+
+      // Trigger loading of next question.
+      Template.instance().currentQuestionIdx.set(currentQuestionIdx + 1);
+    }
+  }
+});
+
+// Take Quiz Question partial template events.
+Template.quizQuestion.events({
+  'change input:radio': function (event, template) {
+    if (Session.get('disableNextStepButton')) {
+      Session.set('disableNextStepButton', false);
+    }
+  }
+});
+
+// Take Quiz Results view template helpers.
+Template.results.helpers({
+  totalCorrectAnswers: function () {
+    return Session.get('numCorrectAnswers');
+  },
+  totalQuestions: function () {
+    var self = this;
+    return self.questions && self.questions.length;
+  }
+});
+
+// Take Quiz Results view template events.
+Template.results.events({
+  'click button': function () {
+    var self = this;
+    Router.go('quiz', { _id: self._id });
   }
 });
--- client/routes.js
+++ client/routes.js
@@ -15,5 +15,22 @@ Router.route('/', {
   }
 });
 
+// Take Quiz view route.
+Router.route('/quiz/:_id', {
+  name: 'quiz',
+  template: 'quiz',
+  data: function () {
+    var self = this;
+    return Quizzes.findOne({ _id: self.params._id });
+  }
+});
+
+// Take Quiz Results view route.
+Router.route('/quiz/:_id/results', {
+  name: 'quiz-results',
+  template: 'results',
+  data: function () {
+    var self = this;
+    return Quizzes.findOne({ _id: self.params._id });
   }
 });

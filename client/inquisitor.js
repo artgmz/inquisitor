3f0db0a239db76ac4febb31eadbdf449c8223097
Arturo Gomez <artgmz@gmail.com>
2015-12-12 22:36:14 -0800

    Create home and quiz card templates to display quiz data in root route.

--- client/inquisitor.js
+++ client/inquisitor.js
@@ -1,15 +1,7 @@
-// counter starts at 0
-Session.setDefault('counter', 0);
-
-Template.home.helpers({
-  counter: function () {
-    return Session.get('counter');
-  }
-});
-
-Template.home.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
+// Quiz Card partial template helpers.
+Template.quizCard.helpers({
+  questionCount: function () {
+    var self = this;
+    return self.questions.length;
   }
 });

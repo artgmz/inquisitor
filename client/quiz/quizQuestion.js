7fe5dc3ccde57cd6fd6c36e76338899078f14526
Arturo Gomez <artgmz@gmail.com>
2015-12-15 03:43:58 -0800

    Rename question to quizQuestion for better organization.

--- client/quiz/quizQuestion.js
+++ client/quiz/quizQuestion.js
@@ -1,5 +1,5 @@
-// Question partial template events.
-Template.question.events({
+// Quiz Question partial template events.
+Template.quizQuestion.events({
   'change input:radio': function (event) {
     // Enable the next step button after the user selects an answer.
     if (Session.get('disableNextStepButton')) {

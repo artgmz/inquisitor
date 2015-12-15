e05c5e8d0fecc2e00f96f26e85e01076d64b994a
Arturo Gomez <artgmz@gmail.com>
2015-12-14 22:01:29 -0800

    Create dummy users to be loaded if there are no users in the database on startup.

--- server/main.js
+++ server/main.js
@@ -3,6 +3,17 @@ Meteor.startup(function () {
   var basicArithmeticQuiz = {};
   var nflTriviaQuiz = {};
   var quizzes = [];
+  var users = [];
+
+  // Seed the database with sample users if none are found.
+  if (Meteor.users.find().count() === 0) {
+    users = [
+      JSON.parse(Assets.getText('users/bendingunit154.json')),
+      JSON.parse(Assets.getText('users/capt_leela.json')),
+      JSON.parse(Assets.getText('users/philip-j-fry.json')),
+      JSON.parse(Assets.getText('users/prof.json'))
+    ];
+  }
 
   // Seed the database with sample quizzes if none are found.
   if (Quizzes.find().count() === 0) {

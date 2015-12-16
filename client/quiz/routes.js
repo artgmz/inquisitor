aa01cc99b26fb32df5206fdfbc2154d594a5634b
Arturo Gomez <artgmz@gmail.com>
2015-12-15 22:14:44 -0800

    Update comment for clarity.

--- client/quiz/routes.js
+++ client/quiz/routes.js
@@ -33,7 +33,7 @@ Router.route('/quiz/:_id/take', {
   data: function () {
     var self = this;
 
-    // We don't a quiz to update while the user is taking it.
+    // We don't want a quiz to update while the user is taking it.
     return Quizzes.findOne({ _id: self.params._id }, { reactive: false });
   }
 });
--- client/quizResult/routes.js
+++ client/quizResult/routes.js
@@ -5,7 +5,7 @@ Router.route('/quiz/:_id/result', {
   data: function () {
     var self = this;
 
-    // We don't a quiz to update while the user is taking it.
+    // We don't want a quiz to update while the user is taking it.
     return Quizzes.findOne({ _id: self.params._id }, { reactive: false });
   }
 });

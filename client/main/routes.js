9e529ef4afeb96cb9e0626137b8ced3ffd28b666
Arturo Gomez <artgmz@gmail.com>
2015-12-15 23:54:52 -0800

    Further clarifying comments.

--- client/main/routes.js
+++ client/main/routes.js
@@ -2,7 +2,7 @@
 Router.configure({
   layoutTemplate: 'main',
   onAfterAction: function () {
-    // We set the title for all pages here because Iron Router prepends the <head>
+    // Set the title for all pages here because Iron Router prepends the <head>
     // tag internally, so we can't set the title as we normally would.
     document.title = 'inquisitor!';
   }
